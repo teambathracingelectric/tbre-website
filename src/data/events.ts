a1d8cccf566138806163623a83efd75f728164f6
Tom Heaton <thomas.m.heaton@gmail.com>
2024-05-15 13:44:04 +0100

    events

--- src/data/events.ts
+++ src/data/events.ts
@@ -1,15 +1,29 @@
 type Event = {
   name: string;
   date: Date;
+  endDate?: Date;
   type?: string;
 };
 
-// TODO: get list of upcoming events
+// TODO: add duration to these events
 
 const eventsData: Event[] = [
   {
     name: "FS UK",
-    date: new Date("2024-07-12"),
+    date: new Date("2024-07-17"),
+    endDate: new Date("2024-07-24"),
+    type: "Competition",
+  },
+  {
+    name: "FS Germany",
+    date: new Date("2024-08-12"),
+    endDate: new Date("2024-08-18"),
+    type: "Competition",
+  },
+  {
+    name: "FS Portugal",
+    date: new Date("2024-09-03"),
+    endDate: new Date("2024-09-07"),
     type: "Competition",
   },
 ];
